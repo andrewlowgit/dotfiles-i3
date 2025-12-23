da52abb92b6f6933b31c1beae36d121881b97108
Andrew <andrew+git@andrewlow.com.au>
2025-12-23 22:02:28 +1000

    Initial dotfiles commit - i3, polybar, fish, and all configs

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,11 @@
+# i3 and system
+alias i3config='nano ~/.config/i3/config'
+
+# General shortcuts
+
+# System updates (adjust for your distro)
+alias update='sudo pacman -Syu'  # For Arch-based
+
+# Navigation
+
+# Git shortcuts (if you use git)
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# Load aliases
+source ~/.config/fish/aliases.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
